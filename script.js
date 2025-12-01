5c2c4deefdc54364ae5a3700cbddcd25ae83131e
mely <123021569+melyjely@users.noreply.github.com>
2025-12-01 14:27:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // Final Project: Checkpoint #1 - Proposal and Prototype
-// 
-// For my final project I want to create a reading log by using react components. This will build off 
-// a to do list example. There will be a form on the page titled, “Log Book” that allows the user to 
+//
+// For my final project I want to create a reading log by using react components. This will build off
+// a to do list example. There will be a form on the page titled, “Log Book” that allows the user to
 // submit the title of the book, author, date finished, a rating, and the ability to favorite the title
-// or not using different HTML input types. After clicking the submit button the form will be submitted, 
+// or not using different HTML input types. After clicking the submit button the form will be submitted,
 // and the log will appear in a list below the form titled, “Books Read.” I would like to be able to
 // filter the list by rating, alphabetizing the title or author, and the date finished. The rating
-// feature will utilize the input of type either range or radio. I would also like to apply CSS to 
-// the form to make the page look more interesting and welcoming, ideally the theme would be able to 
-// toggle from light to dark mode for user preference. If I have time, I could add a couple layers 
-// to the experience. Instead of just logging books that the user has finished and adding a rating, 
+// feature will utilize the input of type either range or radio. I would also like to apply CSS to
+// the form to make the page look more interesting and welcoming, ideally the theme would be able to
+// toggle from light to dark mode for user preference. If I have time, I could add a couple layers
+// to the experience. Instead of just logging books that the user has finished and adding a rating,
 // I would first log the title and author on a “to be read” list with a checkbox, and once the book
-// is read, the user will mark the checkbox and the item will appear on a separate list below (or maybe 
+// is read, the user will mark the checkbox and the item will appear on a separate list below (or maybe
 // to the right of) the first list, titled “read,” where the user will then be able to give a rating for
 // the book and the date it was finished will appear as well. It would also be nice to be able to customize
-// the reading log by allowing the user to input a name, change the color palette of the page, and add 
-// a background. Or alternatively, there could be a few different pre-built themes the user could toggle through.
+// the reading log by allowing the user to input a name, change the color palette of the page, and add
+// a background. Or alternatively, there could be a few different pre-built themes the user could toggle
+// through.
 
-// This project is based on the code for a React To Do List tutorial video.
+// This project is based on the code for a React To Do List tutorial video from Web Dev Simplified.
 // https://www.youtube.com/watch?v=Rh3tobg7hEo
+// https://www.youtube.com/@WebDevSimplified
