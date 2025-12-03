820d16a03afb01c5d12ffcf34d6e5b035775bc17
mely <123021569+melyjely@users.noreply.github.com>
2025-12-03 15:15:46 -0600

    Additional comments

--- src/BookEntry.jsx
+++ src/BookEntry.jsx
@@ -1,10 +1,8 @@
 export function BookEntry({ id, title, author, rating, date, deleteBook }) {
   return (
     <li>
-      {/* Tutorial START */}
       &#x2714;
       <label>{title}</label>
-      {/* Tutorial END */}
       <label> by {author}</label>
       <label>
         {" "}
@@ -14,7 +12,6 @@ export function BookEntry({ id, title, author, rating, date, deleteBook }) {
         {" "}
         Rating: <strong>{rating}</strong>/5
       </label>
-      {/* date format changes when logged */}
       <button onClick={() => deleteBook(id)} className="btn btn-delete">
         &#x2715;
       </button>
--- src/NewBookLog.jsx
+++ src/NewBookLog.jsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
 
 export function NewBookLog({ onSubmit }) {
+  // property destructured = { onSubmit }
+
+  // changing the JSX = declaring what the app will do
+  // In order to modify variables you need to use state
+
+  // useState = hook
+  // - takes a default value -> an empty string ""
+  // - two values: what the value of the thing is & a function for updating value
+  // - cannot update a state variable the way we normally would in JS & also it is a const
+  // - update the input when it changes *see line 37*
+  // - when you change a variable the component is rerendered every time
+
   const [newBook, setNewBook] = useState("");
 
   const [author, setAuthor] = useState("");
@@ -12,18 +24,25 @@ export function NewBookLog({ onSubmit }) {
   function handleSubmit(e) {
     e.preventDefault();
     if (newBook === "") return; // if empty string, then return / do nothing
-    // onSubmit(newBook, author);
+
+    // prop called
     onSubmit(newBook, author, rating, date); // create log for the book in the list
 
     setNewBook(""); // erase fields for next entry
   }
   return (
     <form onSubmit={handleSubmit} className="new-book-form">
+      {/* "class" cannot be used here since it is a reserved keyword so we need to replace that js keyword with "className" */}
       <div className="form-row">
+        {/* React components can only return one element, if you was to return multiple, you can wrap everything in a div or a fragment. Ex. "<></>" */}
         <h1>Log your Books</h1>
+
+        {/* normally would use "for" but since we are using JSX we use "htmlFor" */}
         <label htmlFor="book">Title</label>
         <input
           value={newBook}
+          // onChange -> every time a key is pressed calls this
+          // when a key is pressed, newBook is set to the value in the field rather than staying as an empty string
           onChange={(e) => setNewBook(e.target.value)}
           type="book"
           id="book"
