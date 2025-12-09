953ca34c9faea2ae8a08e5d38bdfdcadd63c8ff0
mely <123021569+melyjely@users.noreply.github.com>
2025-12-08 18:31:12 -0600

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useEffect, useState } from "react";
 import { BookList } from "./BookList";
 import { NewBookLog } from "./NewBookLog";
 
+
 // Tutorial START
+// React components are created with functions that begin with a capital letter
 export default function App() {
+  // function version of useState, whatever you return is your default value
+  // pass a function instead
+  // use saved storage
+  // checking local storage & getting value if it exists
+  // else defaults to an empty arr
+  // hooks need to be called at the top of the function!!!!
   const [books, setBooks] = useState(() => {
     const localValue = localStorage.getItem("ITEMS");
     if (localValue == null) return [];
@@ -13,9 +21,19 @@ export default function App() {
 
   // Notes:
   // storing information in local storage
-  // hooks need to be called at the top of the function
   // always runs the same # of hooks
+  // hooks need to be called at the top of the function!!!!
   // cannot be in conditional statements, for loops, ifs
+  // format is usually:
+  // 1. hooks
+  // 2. helper functions/parsing of data
+  // 3. return JSX
+  // Save the current list to local storage
+
+  // useEffect
+  // - doesn't return anything
+  // - take a function as an arg
+  // - "run this func every time the objects in the arr of second property change"
 
   // Save the current list to local storage
   useEffect(() => {
@@ -24,11 +42,14 @@ export default function App() {
 
   // END TUTORIAL CODE
 
+  // this function needs to be in the App component because the books state is here (line 15)
   function addBook(title, author, rating, date) {
     setBooks((currentBooks) => {
       // return the current list + the new entry
+      // pass a function to the set state & function return the the value you want the new state to be
+      // takes one argument - the current value
       return [
-        ...currentBooks,
+        ...currentBooks, 
         {
           id: crypto.randomUUID(),
           title,
@@ -40,8 +61,9 @@ export default function App() {
     });
   }
 
-  // remove book entry
+  // remove book entry from list
   function deleteBook(id) {
+    // goes through all the books and finds the one that needs to be deleted
     setBooks((currentBooks) => {
       return currentBooks.filter((book) => book.id !== id);
     });
@@ -50,7 +72,10 @@ export default function App() {
   return (
     <>
       {/* form to log book */}
+      {/* import component, rendered almost as if normal html */}
+      {/* custom component syntax: capital letter */}
       <NewBookLog onSubmit={addBook} />
+      {/* prop = onSubmit */}
 
       <div id="paperList">
         <h1 className="header">Book Log</h1>
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -4,13 +4,17 @@ export function BookList({ books: books, deleteBook }) {
   
   return (
     <ul className="list">
+      {/* short circuiting */}
       {books.length === 0 && "Nothing logged yet!"}
 
-      {/* add logged book to list */}
+      {/* loops through and adds books into a map */}
       {books.map((book) => {
         return (
           <BookEntry
+          // pass the properties of book spread out instead of writing "book.title" & "book.id"
             {...book}
+            // need a key (unique identifier) so that react knows what book to update
+            // performance optimization
             key={book.id}
             
             deleteBook={deleteBook}
